d643d36d4e76956aeb670afbeb59eb4f4e73d6ac
Sagar <sagarpatell.1708@gmail.com>
2022-01-15 16:45:06 -0500

    outline done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,115 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-4" style="background-color:hsl(31, 77%, 52%)">
+          <div class="p-5">
+            <div class="container">
+              <div class="row">
+                <div class="p-4">
+                  <img src="/assets/icon-sedans.svg">
+                </div>
+              </div>
+              <div class="row">
+                <div class="p-4">
+                  <span class="text-white" style="font-family:Lexend Deca; font-size:35px">
+                    SEDANS
+                  </span>
+          
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12 text-left p-4">
+                <span class="text-white">
+                Choose a sedan for its affordability and excellent fuel economy.
+                </span>
+
+                </div>
+              </div>
+              <div class="row">
+                <div class="p-4">
+                <button class="text-warning" style="border-radius: 30px;">Learn more</button>
+                
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-4" style="background-color:hsl(184, 100%, 22%)">
+          <div class="p-5">
+            <div class="container">
+              <div class="row">
+                <div class="p-4">
+                <img src="/assets/icon-suvs.svg" alt="">
+                </div>
+              </div>
+              <div class="row">
+                <div class="p-4">
+                  <span class="text-white" style="font-family:Lexend Deca; font-size:35px">
+                    SUVs
+                  </span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12 text-left p-4">
+                <span class="text-white">
+                  Take an SUV for its spacious interior, power and versatility.
+                </span>
+
+                </div>
+              </div>
+              <div class="row">
+                <div class="p-4">
+                <button class="text-info" style="border-radius: 30px">Learn more</button>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+        
+
+        <div class="col-4" style="background-color:hsl(179, 100%, 13%)">
+          <div class="p-5">
+            <div class="container">
+              <div class="row">
+                <div class="p-4">
+                  <img src="/assets/icon-luxury.svg" alt="">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12 text-left p-4">
+                  <span class="text-white" style="font-family: Lexend Deca; font-size:35px">
+                    LUXURY
+                  </span>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12 text-left p-4">
+                  <span class="text-white">
+                    Cruise in the best car brands without the bloated prices.
+                  </span>
+
+                </div>
+              </div>
+              <div class="row">
+                <div class="p-4">
+               <button class="text-primary" style="border-radius: 30px">Learn more</button>
+
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+      </div>
+    </div>
   </div>
+        
+      
+    
+
 </template>
 
 <script>
@@ -55,4 +137,9 @@ li {
 a {
   color: #42b983;
 }
+.text-white {
+  text-align: left;
+  font-family: Big Shoulders Display;
+  }
+  
 </style>
